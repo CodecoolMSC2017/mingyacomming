30c4f0c26a05804f63632ae32015e29a0935912b
prokajkristof <kiscoloshun@gmail.com>
2018-05-15 11:02:59 +0200

    edited schedule database and created service for it

--- src/main/java/com/codecool/web/dao/database/impl/ScheduleDao.java
+++ src/main/java/com/codecool/web/dao/database/impl/ScheduleDao.java
@@ -4,6 +4,8 @@ import com.codecool.web.dao.database.ScheduleDatabase;
 import com.codecool.web.model.Schedule;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ScheduleDao extends AbstractDao implements ScheduleDatabase {
 
@@ -13,13 +15,13 @@ public class ScheduleDao extends AbstractDao implements ScheduleDatabase {
     }
 
     @Override
-    public void addSchedule(Schedule schedule) throws SQLException {
-        String sql = "INSERT into schedule (id, name, user_id) VALUES(?,?,?)";
+    public int addSchedule(Schedule schedule) throws SQLException {
+        String sql = "INSERT into schedules (name, user_id) VALUES(?,?)";
         try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setString(1, schedule.getName());
-            ps.setInt(2, schedule.getId());
-            ps.setInt(3, schedule.getUserId());
-            ps.executeQuery();
+            ps.setInt(2, schedule.getUserId());
+            ResultSet resultSet = ps.executeQuery();
+            return resultSet.getInt("id");
         }
     }
 
@@ -27,7 +29,7 @@ public class ScheduleDao extends AbstractDao implements ScheduleDatabase {
     public void removeSchedule(Schedule schedule) throws SQLException {
         boolean autoCommit = connection.getAutoCommit();
         connection.setAutoCommit(false);
-        String sql = "DELETE FROM schedule WHERE id = ?";
+        String sql = "DELETE FROM schedules WHERE id = ?";
         try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);) {
             statement.setInt(1, schedule.getId());
             statement.executeUpdate();
@@ -42,7 +44,7 @@ public class ScheduleDao extends AbstractDao implements ScheduleDatabase {
 
     @Override
     public Schedule getSchedule(int id) throws SQLException {
-        String sql = "SELECT * FROM schedule WHERE id = ?";
+        String sql = "SELECT * FROM schedules WHERE id = ?";
         try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setInt(1, id);
             try(ResultSet rs = ps.executeQuery()) {
@@ -52,5 +54,38 @@ public class ScheduleDao extends AbstractDao implements ScheduleDatabase {
                 return null;
             }
         }
+    }
+
+    @Override
+    public List<Schedule> getSchedules() throws SQLException {
+        String sql = "SELECT * FROM schedules";
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+            List<Schedule> schedules = new ArrayList<>();
+            while (resultSet.next()) {
+                schedules.add(fetchSchedule(resultSet));
+            }
+            return schedules;
+        }
+    }
+
+    @Override
+    public List<Schedule> getUserSchedule(int id) throws SQLException {
+        String sql = "SELECT * FROM schedules WHERE user_id = ?";
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+            List<Schedule> schedules = new ArrayList<>();
+            while (resultSet.next()) {
+                schedules.add(fetchSchedule(resultSet));
+            }
+            return schedules;
+        }
+    }
+
+    private Schedule fetchSchedule(ResultSet resultSet) throws SQLException {
+        int id = resultSet.getInt("id");
+        String name = resultSet.getString("name");
+        int user_id = resultSet.getInt("user_id");
+        return new Schedule(id, name, user_id);
     }
 }
