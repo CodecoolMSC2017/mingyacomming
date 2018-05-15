b9fb77fde85d22cecad3e857eb75a4a015eab4af
prokajkristof <kiscoloshun@gmail.com>
2018-05-15 11:34:32 +0200

    added Service for Slot

---
+++ src/main/java/com/codecool/web/service/SlotService.java
@@ -0,0 +1,17 @@
+package com.codecool.web.service;
+
+import com.codecool.web.model.Slot;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface SlotService {
+
+    int addSlot(Slot slot) throws SQLException;
+
+    void removeSlot(Slot slot) throws SQLException;
+
+    Slot getSlot(int id) throws SQLException;
+
+    List<Slot> getDaySlots(int id) throws SQLException;
+}
