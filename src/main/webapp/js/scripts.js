d9b1ad177da77be29e47b80cfc62bb63e821a5ea
S. Dominik <coderwilk@gmail.com>
2018-05-15 11:16:31 +0200

    Added some functions for handling messages

--- src/main/webapp/js/scripts.js
+++ src/main/webapp/js/scripts.js
@@ -5,12 +5,14 @@ function login() {
   const logData = getLogFields();
 
   let xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", changeUserTab);
   xhr.open("POST", `${BASE_URL}/login`);
   xhr.send(JSON.stringify(logData));
 }
 
 function getLogFields() {
   let logData = {};
+
   let usernameE = document.getElementById("log_username_field");
   logData.username = usernameE.value;
 
@@ -26,7 +28,7 @@ function register() {
   const regData = getRegFields();
 
   let xhr = new XMLHttpRequest();
-  xhr.addEventListener("load", createMessage);
+  xhr.addEventListener("load", checkResp);
   xhr.open("POST", `${BASE_URL}/register`);
   xhr.send(JSON.stringify(regData));
 }
@@ -43,18 +45,68 @@ function getRegFields() {
   return regData;
 }
 
-// User-Tab method(s)
+
+// UserTab
+function changeUserTab() {
+  const message = JSON.parse(this.responseText);
+  createMessage(message);
+
+  if (message.response != 200) {
+    return;
+  }
+  console.log("logged in");
+  document.getElementById("login_form").style.display = "none";
+}
+
+function getUserData() {
+  let xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", );
+  xhr.open("POST", `${BASE_URL}/register`);
+  xhr.send(JSON.stringify(regData));
+}
+
+// Create task method(s)
+function createTask() {
+  const taskData = getCreateTaskFields();
+
+  let xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", checkResp);
+  xhr.open("POST", `${BASE_URL}/tasks`);
+  xhr.send(JSON.stringify(taskData));
+}
+
+function getCreateTaskFields() {
+  let taskData = {};
+
+  let nameE = document.getElementById("create_task_name_field");
+  taskData.name = nameE.value;
+
+  let descriptionE = document.getElementById("create_task_description_field");
+  taskData.description = descriptionE.value;
+
+  return taskData;
+}
+
 
 // Message method(s)
-function createMessage() {
-  console.log(this);
-  console.log(this.headers);
+function checkResp() {
+  const message = JSON.parse(this.responseText);
+  console.log(message);
+}
+
+function createMessage(message) {
+  console.log(message);
+}
+
+function addMessage(content) {
+  
 }
 
 // Core
 function init() {
   document.getElementById("log_button").addEventListener("click", login);
   document.getElementById("reg_button").addEventListener("click", register);
+  document.getElementById("create_task_button").addEventListener("click", createTask);
 
   document.getElementById("log_tab").addEventListener("click", () => {
     document.getElementById("login_form").style.display = "block";
@@ -63,6 +115,15 @@ function init() {
   document.getElementById("reg_tab").addEventListener("click", () => {
     document.getElementById("register_form").style.display = "block";
     document.getElementById("login_form").style.display = "none";
+  });
+
+  document.getElementById("game_button").addEventListener("click", () => {
+    let content = document.getElementById("content");
+    if (content.style.display == "none") {
+      content.style.display = "block";
+    } else {
+      content.style.display = "none";
+    }
   });
 }
 
