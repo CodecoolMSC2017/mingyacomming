7bf1da801dfdcf0dec4422c8452af6309c9ce088
gaboroneszabo <puma678@gmail.com>
2018-05-16 14:20:43 +0200

    add tests for DayDao, ScheduleDao

---
+++ src/test/java/com/codecool/web/dao/database/impl/AbstractTest.java
@@ -0,0 +1,34 @@
+package com.codecool.web.dao.database.impl;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public abstract class AbstractTest {
+
+    private String getQueryString() {
+        Scanner scanner = new Scanner(AbstractTest.class.getResourceAsStream("/init.sql"));
+        String sql = "";
+        while (scanner.hasNext()) {
+            sql += scanner.nextLine();
+        }
+        return sql;
+    }
+
+    public void runInitStript(Connection connection) throws FileNotFoundException, SQLException {
+        try(Statement st = connection.createStatement()) {
+            st.execute(getQueryString());
+        }
+    }
+
+    public Connection getConnection() throws SQLException {
+        String USER = System.getProperty("dbUser", "postgres");
+        String PASS = System.getProperty("dbPass", "admin");
+        String DB_URL = System.getProperty("dbUrl", "jdbc:postgresql://localhost:5432/mingyagyuvok");
+        return DriverManager.getConnection(DB_URL, USER, PASS);
+    }
+}
