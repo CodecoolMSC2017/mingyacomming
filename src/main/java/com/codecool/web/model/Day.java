439087bf85b7387e1ce7a4d203f1153e07671b42
gaboroneszabo <puma678@gmail.com>
2018-05-16 13:32:44 +0200

    change user_id file to schedule_id

--- src/main/java/com/codecool/web/model/Day.java
+++ src/main/java/com/codecool/web/model/Day.java
@@ -6,18 +6,18 @@ import java.util.List;
 public class Day {
 
     private int id;
-    private int user_id;
+    private int schedule_id;
     private String name;
     private List<Slot> slots = new ArrayList<>();
 
-    public Day(int id, String name, int user_id) {
+    public Day(int id, String name, int schedule_id) {
         this.id = id;
-        this.user_id = user_id;
+        this.schedule_id = schedule_id;
         this.name = name;
     }
 
-    public Day(String name, int user_id) {
-        this.user_id = user_id;
+    public Day(String name, int schedule_id) {
+        this.schedule_id = schedule_id;
         this.name = name;
     }
 
@@ -33,8 +33,8 @@ public class Day {
         this.name = name;
     }
 
-    public int getUser_id() {
-        return user_id;
+    public int getSchedule_id() {
+        return schedule_id;
     }
 
     public List<Slot> getSlots() {
