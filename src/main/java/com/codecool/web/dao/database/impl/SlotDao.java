f002f9d16a5376f3033d88079de59ae5640f52c9
prokajkristof <kiscoloshun@gmail.com>
2018-05-17 15:25:10 +0200

    rák

--- src/main/java/com/codecool/web/dao/database/impl/SlotDao.java
+++ src/main/java/com/codecool/web/dao/database/impl/SlotDao.java
@@ -102,8 +102,8 @@ public class SlotDao extends AbstractDao implements SlotDatabase {
     }
 
     public List<SlotTask> getSlotTask(int id) throws SQLException {
-        String sql = "Select s.id, s.time, s.task_id, s.day_id, t.name, t.description, t.user_id\n" +
-                "From slots as s Inner Join tasks as t ON s.task_id = t.id WHERE day_id = ?";
+        String sql = "Select s.id, s.time, s.task_id, s.day_id, t.name, t.description, t.user_id \n" +
+                "                From slots as s Inner Join tasks as t ON s.task_id = t.id WHERE day_id = 1";
         try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setInt(1, id);
             try(ResultSet rs = ps.executeQuery()) {
