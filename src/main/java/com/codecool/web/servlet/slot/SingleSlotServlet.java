f32c14699a4029e887e71a88339478baf9b0e81e
prokajkristof <kiscoloshun@gmail.com>
2018-05-31 13:59:51 +0200

    Added logger for slot servlets

--- src/main/java/com/codecool/web/servlet/slot/SingleSlotServlet.java
+++ src/main/java/com/codecool/web/servlet/slot/SingleSlotServlet.java
@@ -6,6 +6,8 @@ import com.codecool.web.model.Slot;
 import com.codecool.web.service.SlotService;
 import com.codecool.web.service.impl.SimpleSlotService;
 import com.codecool.web.servlet.AbstractServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,6 +21,8 @@ import java.sql.SQLException;
 @WebServlet("/slots/*")
 public class SingleSlotServlet extends AbstractServlet{
 
+    private static final Logger logger = LoggerFactory.getLogger(SingleSlotServlet.class);
+
     private static int getId(HttpServletRequest req) {
         String uri = req.getRequestURI();
         int idx = uri.lastIndexOf('/');
@@ -27,6 +31,7 @@ public class SingleSlotServlet extends AbstractServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.trace("Started to get slot");
         try {
             int id = getId(req);
 
@@ -36,19 +41,23 @@ public class SingleSlotServlet extends AbstractServlet{
                 SlotService ts = new SimpleSlotService(sdb);
 
                 sendMessage(resp, 200, ts.getDaySlots(id));
+                logger.info("Slot got succesfully");
 
             } catch (SQLException e) {
                 sendMessage(resp, 400, "bad id");
+                logger.error("Error in getting slot", e);
             }
         }
 
         catch (NumberFormatException e) {
             sendMessage(resp, 400, e.getMessage());
+            logger.error("Error in getting slot", e);
         }
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.trace("Started to delete slot");
         try {
             int id = getId(req);
 
@@ -60,18 +69,22 @@ public class SingleSlotServlet extends AbstractServlet{
                 slotService.removeSlot(slotService.getSlot(id));
 
                 sendMessage(resp, 200, "deleted");
+                logger.info("Slot deleted succesfully");
 
             } catch (SQLException e) {
                 sendMessage(resp, 400, "bad id");
+                logger.error("Error in deleting slot", e);
             }
         }
         catch (NumberFormatException e) {
             sendMessage(resp, 400, e.getMessage());
+            logger.error("Error in deleting slot", e);
         }
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.trace("Started editing slot");
         String jsonString = req.getReader().readLine();
         int id = getId(req);
 
@@ -92,10 +105,12 @@ public class SingleSlotServlet extends AbstractServlet{
             slotService.updateSlot(slot);
 
             sendMessage(resp, 200, "Slot updated succesfully");
+            logger.info("Slot edited succesfully");
 
         } catch (SQLException e) {
             e.printStackTrace();
             sendMessage(resp, 400, "something went wrong");
+            logger.error("Error in editing slot", e);
         }
     }
 }
--- src/main/java/com/codecool/web/servlet/slot/SlotsServlet.java
+++ src/main/java/com/codecool/web/servlet/slot/SlotsServlet.java
@@ -6,6 +6,8 @@ import com.codecool.web.model.Slot;
 import com.codecool.web.service.SlotService;
 import com.codecool.web.service.impl.SimpleSlotService;
 import com.codecool.web.servlet.AbstractServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,9 +20,11 @@ import java.sql.SQLException;
 @WebServlet("/slots")
 public class SlotsServlet extends AbstractServlet {
 
+    private static final Logger logger = LoggerFactory.getLogger(SlotsServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        logger.trace("Started getting slots");
         int id = Integer.parseInt(req.getParameter("dayId"));
 
         try {
@@ -30,20 +34,24 @@ public class SlotsServlet extends AbstractServlet {
                 SlotService ss = new SimpleSlotService(sdb);
 
                 sendMessage(resp, 200, ss.getSlotTask(id));
+                logger.info("Slots got succesfully");
 
             } catch (SQLException e) {
                 e.printStackTrace();
                 sendMessage(resp, 500, "sqlserver is down");
+                logger.error("Error in getting slots" ,e);
             }
         }
         catch (NumberFormatException e) {
             e.printStackTrace();
             sendMessage(resp, 400, "error in dayID");
+            logger.error("Error in getting slots", e);
         }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.trace("Started to create new slot");
         String jsonString = req.getReader().readLine();
 
         String id = req.getParameter("dayId");
@@ -61,13 +69,16 @@ public class SlotsServlet extends AbstractServlet {
                 int slotId = ss.addSlot(new Slot(0, slotTime, taskId, dayId));
                 String message = "slots/" + slotId;
                 sendMessage(resp, 200, message);
+                logger.info("slot created succesfully");
 
             } catch (SQLException e) {
                 sendMessage(resp, 500, "sqlserver is down");
+                logger.error("error in creating slot", e);
             }
         }
         catch (NumberFormatException e) {
             sendMessage(resp, 400, "error converting numbers");
+            logger.error("error in creating slot", e);
         }
     }
 }
