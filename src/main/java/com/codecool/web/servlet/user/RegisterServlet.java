70b8f82309b7b813b673a4373e23fc8e80220a9c
prokajkristof <kiscoloshun@gmail.com>
2018-05-31 14:00:10 +0200

    Added logger for user servlets

--- src/main/java/com/codecool/web/servlet/user/RegisterServlet.java
+++ src/main/java/com/codecool/web/servlet/user/RegisterServlet.java
@@ -8,8 +8,11 @@ import com.codecool.web.exceptions.UserNameException;
 import com.codecool.web.service.RegisterService;
 import com.codecool.web.service.impl.SimpleRegisterService;
 import com.codecool.web.servlet.AbstractServlet;
+import com.codecool.web.servlet.slot.SingleSlotServlet;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -22,9 +25,11 @@ import java.sql.SQLException;
 @WebServlet("/register")
 public class RegisterServlet extends AbstractServlet {
 
+    private static final Logger logger = LoggerFactory.getLogger(RegisterServlet.class);
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        logger.trace("Started register");
         String jsonString = req.getReader().readLine();
 
         String userName = getJsonParameter("username", jsonString);
@@ -35,13 +40,17 @@ public class RegisterServlet extends AbstractServlet {
             RegisterService rs = new SimpleRegisterService(ud);
             rs.register(userName, password);
             sendMessage(resp, 200, "user registered");
+            logger.info("Registered succesfully");
 
         } catch (SQLException e) {
             handleSqlError(resp, e);
+            logger.error("Error in register", e);
         } catch (UserAlreadyExistException e) {
             sendMessage(resp, 400, "user already exists");
+            logger.error("Error in register", e);
         } catch (UserNameException e) {
             sendMessage(resp, 400, "user cannot be null");
+            logger.error("Error in register", e);
         }
     }
 }
--- src/main/java/com/codecool/web/servlet/user/UsersServlet.java
+++ src/main/java/com/codecool/web/servlet/user/UsersServlet.java
@@ -4,6 +4,8 @@ import com.codecool.web.dao.database.UserDatabase;
 import com.codecool.web.dao.database.impl.UserDao;
 import com.codecool.web.model.User;
 import com.codecool.web.servlet.AbstractServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,14 +19,19 @@ import java.util.List;
 @WebServlet("/users")
 public class UsersServlet extends AbstractServlet {
 
+    private static final Logger logger = LoggerFactory.getLogger(UsersServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.trace("Started getting users");
         try(Connection connection = getConnection(req.getServletContext())) {
             UserDatabase userDatabase = new UserDao(connection);
             List<User> users = userDatabase.getAllUser();
             sendMessage(resp, 200, users);
+            logger.info("Users got succesfully");
         } catch (SQLException e) {
             sendMessage(resp, 500, e.getMessage());
+            logger.error("Error in getting users", e);
         }
     }
 }
