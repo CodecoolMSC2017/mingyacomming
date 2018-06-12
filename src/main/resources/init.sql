bbb34345eb6af1a4e2b55ab50af22d6f6d0609ad
gaboroneszabo <puma678@gmail.com>
2018-06-12 13:16:08 +0200

    user password check null removed

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -6,7 +6,7 @@ CREATE TABLE users
 (
     id       SERIAL PRIMARY KEY,
     name     TEXT   UNIQUE NOT NULL,
-    password TEXT   NOT NULL,
+    password TEXT   ,
     role     TEXT   NOT NULL,
     email     TEXT   NOT NULL,
 	CONSTRAINT name_not_empty CHECK (name <> ''),
